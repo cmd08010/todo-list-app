c2085b20e87581662a478d0714dd5948aabb9d87
cmd08010 <colleendunion@gmail.com>
2020-01-22 10:39:07 -0500

    completed add event, cross out, hide all, and show all... Yippeee

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import Todo from "./Components/Todo"
+import "bootstrap/dist/css/bootstrap.css"
 import "./App.css"
 
 function App() {
+  const data = []
+  let [text, setText] = useState("")
+  let [todoList, setTodoList] = useState(data)
+
+  const setInputText = e => {
+    e.preventDefault()
+    setText(document.querySelector("#create-input").value)
+    newTodoListItem()
+  }
+  const newTodoListItem = () => {
+    let newItem = {
+      name: text,
+      completed: "notCompleted",
+      show: "showing"
+    }
+
+    setTodoList([...todoList, newItem])
+  }
+
   return (
     <div className="App">
-      <Todo />
+      <Todo
+        todoList={todoList}
+        onClick={setInputText}
+        setTodoList={setTodoList}
+      />
     </div>
   )
 }
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,7 +1,94 @@
 import React, { useState } from "react"
 
-function Todo() {
-  return null
+function Todo({ todoList, onClick, setTodoList }) {
+  const crossOutToDo = e => {
+    let todoItem = e.target.innerText
+    todoList.forEach(item => {
+      if (item.name === todoItem) {
+        if (item.completed === "completed") {
+          item.completed = "notCompleted"
+        } else if (item.completed === "notCompleted") {
+          item.completed = "completed"
+        }
+        setTodoList([...todoList])
+      }
+    })
+  }
+
+  const hideComplete = e => {
+    e.preventDefault()
+    todoList.forEach(item => {
+      if (item.completed === "completed") {
+        item.completed = "notShowing"
+        setTodoList([...todoList])
+      }
+    })
+    console.log(todoList)
+  }
+
+  const showAll = e => {
+    e.preventDefault()
+    todoList.forEach(item => {
+      if (item.completed === "notShowing") {
+        item.completed = "completed"
+        setTodoList([...todoList])
+      }
+    })
+  }
+
+  // console.log(todoList, "todolist variable should be an array of objects")
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <div className="card">
+        <form>
+          <div>
+            <button
+              id="hideComplete"
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={hideComplete}
+            >
+              Hide Complete
+            </button>
+            <button
+              id="showAll"
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={showAll}
+            >
+              ShowAll
+            </button>{" "}
+          </div>
+          <div>
+            <input id="create-input" type="text" />
+            <button
+              id="create"
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={onClick}
+            >
+              Create
+            </button>
+          </div>
+        </form>
+        <ul>
+          {todoList.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className={item.showing}
+                id={item.completed}
+                onClick={crossOutToDo}
+              >
+                {item.name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Todo
